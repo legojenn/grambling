e0151281540fc5a2fded1e59e663ecd5649f5eae
legojenn <157502156+legojenn@users.noreply.github.com>
2024-01-26 16:59:25 -0500

    Add files via upload

--- play_like_a_pro.py
+++ play_like_a_pro.py
@@ -1,248 +1,138 @@
-#Title:          Roulette Number Predictor for Play Like a Pro
-#Started:        2024-01-22
-#Last Modified:  2024-01-23
-#Python version: 3.10.12
-#Pandas version: 2.1.4
-#Purpose:        Reads input from keyboard for last value spun in roulette
-#                and number of spins since the bet won, max number of spins
-#                between wins and median number of spins.
+# Title:          Roulette Number Predictor for Play Like a Pro/Fibonacci Dozens
+# Version:        2.0.1
+# Started:        2024-01-22
+# Last Modified:  2024-01-26
+# Python version: 3.10.12
+# Pandas version: 2.1.4
+# Purpose:        Reads input from keyboard for last value spun in roulette
+#                 and number of spins since the bet won, max number of spins
+#                 between wins and median number of spins.
 #
-#Note:           The code is completely unoptimised for readability.  
-
-#Load Pandas & NumPy
+# Note:           Code was rewritten to optimise processing 
+temp=[]
+# Import Pandas & NumPy
 import pandas as pd
 import numpy  as np
-import os
+# Import from OS 
+from os import system, name
 
-#set initial values
-series_red     = pd.DataFrame([1,3,5,7,9,12,14,16,18,19,21,23,25,27,30,32,34,36])
-series_black   = pd.DataFrame([2,4,6,8,10,11,13,15,17,20,22,24,26,28,29,31,33,35])
-series_low     = pd.DataFrame([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18])
-series_high    = pd.DataFrame([19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36])
-series_odd     = pd.DataFrame([1,3,5,7,9,11,13,15,17,19,21,23,25,27,29,31,33,35])
-series_even    = pd.DataFrame([2,4,6,8,10,12,14,16,18,20,22,24,26,28,30,32,34,36])
+# Load series of roulette numbers
+series = pd.read_csv("roulette_series.csv")
 
-wins_red        = []
-wins_black      = []
-wins_odd        = []
-wins_even       = []
-wins_low        = []
-wins_high       = []
+# Create lists to save winning numbers 
+# and variables to count losses
+series_names = series.columns.tolist()
+wins   = {}
+losses = {}
+for series_name in series_names:
+    key = str(series_name)
+    losses[key] = 0
+    wins[key] = []
 
-losses_red     = 0
-losses_black   = 0
-losses_low     = 0
-losses_high    = 0
-losses_odd     = 0
-losses_even    = 0
+def print_settings(threshold_1_to_1_print, threshold_2_to_1_print, exclude_below_threshold_print):
+    print("Betting thresholds: " + str(threshold_1_to_1_print) + " for one to one bets and " + str(threshold_2_to_1_print) + " for two to one bets")
+    if(exclude_below_threshold_print):
+        print("Excluding", end = "")
+    else:
+        print("Including", end = "")
+    print(" values below the betting threshold in percentiles.\n")
 
-current_number = 0
+def print_results_table(series_names,calc_percentile_results,threshold_1, threshold_2):
+    print('{0: <15}'.format('Bet'), end = "")
+    print('{0: <22}'.format('Spins Since Last Hit'), end = "")
+    print('{0: <28}'.format('Maximum Spins Between Hits'), end = "")
+    print('{0: <15}'.format(str(calc_percentile_results) + 'th Percentile'))
 
-max_difference = 2
-threshold      = 4
+    print("⎼" * 80)
+    for series_name in series_names:
+        if (len(wins[series_name]) > 0):
+            print('{0: <15}'.format(series_name), end = "")
+            print('{0: >11}'.format(losses[series_name]), end = "")
+            print('{0: >25}'.format(max(wins[series_name])), end = "")
+            temp_list = wins[series_name]
+            if (series[series_name].count() > 12):
+                threshold = threshold_1
+            else:
+                threshold = threshold_2
+            temp_list = [x for x in temp_list if x >= threshold]
+            if(len(temp_list) > 0):        
+                temp = float("{:.1f}".format(np.percentile(temp_list, calc_percentile_results)))
+                print('{0: >25}'.format(str(temp)), end = "")
+            print("")
+            print("-" * 80)
 
-place_holder   = " "
 
-# Clearing the Screen
-os.system('clear') # 'cls' for windows
+def print_suggested_bets(series_names,calc_percentile,threshold_1, threshold_2, max_difference):
 
-
-# Main Loop 
-
-while (current_number !=99):
-    
-    current_number = int(input("Enter the last spin (99 to quit): "))
-
+    print("\nBets at or over threshold:")
+    for series_name in series_names:
+        if (series[series_name].count() > 12):
+            threshold = threshold_1
+        else:
+            threshold = threshold_2
+        if (len(wins[series_name]) > 0):
+            if ((losses[series_name] >= threshold)):
+                print(" " + str(series_name), end = "")
     print(" ")
 
-    if (current_number in series_black.values):
-        wins_black.append(losses_black + 1)
-        losses_black = 0;
-        losses_red += 1;
-    
-    if (current_number in series_red.values):
-        wins_red.append(losses_red + 1)
-        losses_red = 0;
-        losses_black += 1;
-        
-    if (current_number in series_low.values):
-        wins_low.append(losses_low + 1)
-        losses_low = 0;
-        losses_high += 1;  
-    
-    if (current_number in series_high.values):
-        wins_high.append(losses_high + 1)
-        losses_high = 0;
-        losses_low += 1;
-    
-    if (current_number in series_odd.values):
-        wins_odd.append(losses_odd + 1)
-        losses_odd = 0;
-        losses_even += 1;
-        
-    if (current_number in series_even.values):
-        wins_even.append(losses_even + 1)
-        losses_even = 0;
-        losses_odd += 1;
-
-    if (current_number == 0):
-        losses_red   += 1;
-        losses_black += 1;
-        losses_odd   += 1;
-        losses_even  += 1;
-        losses_high  += 1;
-        losses_low   += 1;
-
-    if (len(wins_low) > 0):
-        print("Spins since last 1-18 win  ", end ="")
-        if (losses_low < 10):
-            print (place_holder, end ="")
-        print (str(losses_low), end = "")
-        print(" - Max spins between wins ", end = "") 
-        if (max(wins_low) < 10):
-            print (place_holder, end ="")
-        print (str(max(wins_low)), end = "")
-        print(" - Top quintile : ", end ="")
-        if (max(str(np.percentile(wins_low, 80) < 10))):
-            print (place_holder, end ="")        
-        print(str(float("{:.1f}".format(np.percentile(wins_low, 80)  ))), end = "")
-        print (".")
-
-    if (len(wins_even) > 0):
-        print("Spins since last even win  ", end ="")
-        if (losses_even < 10):
-            print (place_holder, end ="")
-        print (str(losses_even), end = "")
-        print(" - Max spins between wins ", end = "") 
-        if (max(wins_even) < 10):
-            print (place_holder, end ="")
-        print (str(max(wins_even)), end = "")
-        print(" - Top quintile : ", end ="")
-        if (max(str(np.percentile(wins_even, 80) < 10))):
-            print (place_holder, end ="")        
-        print(str(float("{:.1f}".format(np.percentile(wins_even, 80)  ))), end = "")
-        print (".")
-
-    if (len(wins_red) > 0):
-        print("Spins since last red win   ", end ="")
-        if (losses_red < 10):
-            print (place_holder, end ="")
-        print (str(losses_red), end = "")
-        print(" - Max spins between wins ", end = "") 
-        if (max(wins_red) < 10):
-            print (place_holder, end ="")
-        print (str(max(wins_red)), end = "")
-        print(" - Top quintile : ", end ="")
-        if (max(str(np.percentile(wins_red, 80) < 10))):
-            print (place_holder, end ="")        
-        print(str(float("{:.1f}".format(np.percentile(wins_red, 80)  ))), end = "")
-        print (".")
-
-    if (len(wins_black) > 0):
-        print("Spins since last black win ", end ="")
-        if (losses_black < 10):
-            print (place_holder, end ="")
-        print (str(losses_black), end = "")
-        print(" - Max spins between wins ", end = "") 
-        if (max(wins_black) < 10):
-            print (place_holder, end ="")
-        print (str(max(wins_black)), end = "")
-        print(" - Top quintile : ", end ="")
-        if (max(str(np.percentile(wins_black, 80) < 10))):
-            print (place_holder, end ="")        
-        print(str(float("{:.1f}".format(np.percentile(wins_black, 80)  ))), end = "")
-        print (".")
-
-    if (len(wins_odd) > 0):
-        print("Spins since last odd win   ", end ="")
-        if (losses_odd < 10):
-            print (place_holder, end ="")
-        print (str(losses_odd), end = "")
-        print(" - Max spins between wins ", end = "") 
-        if (max(wins_odd) < 10):
-            print (place_holder, end ="")
-        print (str(max(wins_odd)), end = "")
-        print(" - Top quintile : ", end ="")
-        if (max(str(np.percentile(wins_odd, 80) < 10))):
-            print (place_holder, end ="")        
-        print(str(float("{:.1f}".format(np.percentile(wins_odd, 80)  ))), end = "")
-        print (".")
-
-    if (len(wins_high) > 0):
-        print("Spins since last 19-36 win ", end ="")
-        if (losses_high < 10):
-            print (place_holder, end ="")
-        print (str(losses_high), end = "")
-        print(" - Max spins between wins ", end = "") 
-        if (max(wins_high) < 10):
-            print (place_holder, end ="")
-        print (str(max(wins_high)), end = "")
-        print(" - Top quintile : ", end ="")
-        if (max(str(np.percentile(wins_high, 80) < 10))):
-            print (place_holder, end ="")        
-        print(str(float("{:.1f}".format(np.percentile(wins_high, 80)  ))), end = "")
-        print(".")
-
+    print("\nBets over threshold within " + str(max_difference)  + " losses of maximum:")
+    for series_name in series_names:
+        if (series[series_name].count() > 12):
+            threshold = threshold_1
+        else:
+            threshold = threshold_2
+        if (len(wins[series_name]) > 0):
+            if ((max(wins[series_name]) < losses[series_name] + max_difference) and (losses[series_name] >= threshold)):
+                print(" " + str(series_name), end = "")
     print(" ")
-    print("Bets that are greater than its 4th quintile:", end = "")
-    if (len(wins_low) > 0):
-        if (losses_low > np.percentile(wins_low, 80) ):
-            print(" 1-18", end ="")
-    if (len(wins_even) > 0):
-        if (losses_even > np.percentile(wins_even, 80) ):
-            print(" even", end ="")
-    if (len(wins_red) > 0):
-        if (losses_red > np.percentile(wins_red, 80) ):
-            print(" red", end ="")
-    if (len(wins_black) > 0):
-        if (losses_black > np.percentile(wins_black, 80) ):
-            print(" black", end ="")
-    if (len(wins_odd) > 0):
-        if (losses_odd > np.percentile(wins_odd, 80) ):
-            print(" odd", end ="")
-    if (len(wins_high) > 0):
-        if (losses_high > np.percentile(wins_high, 80) ):
-            print(" 19-36", end ="")
-    print(".")
-    print("Bets that are within "+ str(max_difference) + " spins of the max:", end = "")
-    if (len(wins_low) > 0):
-        if ((max(wins_low) - max_difference) <= losses_low ):
-            print(" 1-18", end ="")
-    if (len(wins_even) > 0):
-        if ((max(wins_even) - max_difference) <= losses_even ):
-            print(" even", end ="")
-    if (len(wins_red) > 0):
-        if ((max(wins_red) - max_difference) <= losses_red ):
-            print(" red", end ="")
-    if (len(wins_black) > 0):
-        if ((max(wins_black) - max_difference) <= losses_black ):
-            print(" black", end ="")
-    if (len(wins_odd) > 0):
-        if ((max(wins_odd) - max_difference) <= losses_odd ):
-            print(" odd", end ="")
-    if (len(wins_high) > 0):
-        if ((max(wins_high) - max_difference) <= losses_high ):
-            print(" 19-36", end ="")
-    print(".")
-    print("Bets that are at or over "+ str(threshold) + " losses:", end = "")
-    if (len(wins_low) > 0):
-        if (losses_low >= threshold):
-            print(" 1-18", end ="")   
-    if (len(wins_even) > 0):
-        if (losses_even >= threshold):
-            print(" even", end ="")     
-    if (len(wins_red) > 0):
-        if (losses_red >= threshold):
-            print(" red", end ="")
-    if (len(wins_black) > 0):
-        if (losses_black >= threshold):
-            print(" black", end ="")   
-    if (len(wins_odd) > 0):
-        if (losses_odd >= threshold):
-            print(" odd", end ="")     
-    if (len(wins_high) > 0):
-        if (losses_high >= threshold):
-            print(" 19-36", end ="")
-    print(".")
-    print('\n' * 2)
-print ("Done")
+
+    print("\nBets over threshold and in the " + str(calc_percentile) + "th percentile:")
+    for series_name in series_names:
+        if (series[series_name].count() > 12):
+            threshold = threshold_1
+        else:
+            threshold = threshold_2
+        if (len(wins[series_name]) > 0):
+            temp_list = wins[series_name]
+            temp_list = [x for x in temp_list if x >= threshold]
+            if(len(temp_list) > 0):
+                if (losses[series_name] >= np.percentile(temp_list, calc_percentile)):     
+                    print(" " + str(series_name), end = "")
+    print(" ")
+
+
+def main():
+    # Set global variables
+    current_number          = 0
+    max_difference          = 2
+    threshold_1_to_1        = 4
+    threshold_2_to_1        = 6
+    exclude_below_threshold = True
+    if (exclude_below_threshold == False):
+        threshold_1_to_1    = 0
+        threshold_2_to_1    = 0
+    calc_percentile         = 60
+
+    # Core Loop
+    # Collect spin value- fix it to account for errors
+    while (current_number != 99):
+        print('—' * 80)
+        current_number = int(input("Enter the last spin (99 to quit): "))
+        print('—' * 80)
+        if(current_number != 99):
+            # Check number against group lists
+            # check_values(current_number,series_names) function does not work
+            for series_name in series_names:
+                if (current_number in series[series_name].values):
+                    wins[series_name].append(losses[series_name])
+                    losses[series_name] = 0
+                else: 
+                    losses[series_name] += 1
+
+            #Print statistics
+            print_results_table(series_names,calc_percentile,threshold_1_to_1, threshold_2_to_1)
+            #Print suggestions
+            print_suggested_bets(series_names,calc_percentile,threshold_1_to_1, threshold_2_to_1, max_difference)
+
+if __name__ == "__main__":
+    main()
